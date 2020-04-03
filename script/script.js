e4ba448e8e272a37f562101d239935fae4f710f9
hadiazb <hadiazb@unal.edu.co>
2020-04-02 20:43:19 -0500

    Creando el primer Sliders

--- script/script.js
+++ script/script.js
@@ -3,13 +3,13 @@ var contador = 1;
 function main(){
   $('.boton').click(function(){
     if(contador == 1){
-      $('nav').animate({
+      $('.header__desplegable').animate({
         left:'0'
       });
       contador = 0;
     }else {
       contador = 1;
-      $('nav').animate({
+      $('.header__desplegable').animate({
         left:'-100%'
       });
     }
